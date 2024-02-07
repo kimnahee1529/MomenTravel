aa0f85b6e1f11d94106e7cd093d56dbc3ecfb71c
nhkim <nhkim1529@gmail.com>
2024-02-07 14:46:25 +0900

    Chore: cache clear

---
+++ app/src/main/java/com/android/traveltube/utils/UtilManager.kt
@@ -0,0 +1,12 @@
+//package com.android.traveltube.utils
+//
+//import android.widget.ImageView
+//import com.bumptech.glide.Glide
+//
+//object UtilManager {
+//    fun ImageView.loadImage(url: String){
+//        Glide.with(this)
+//            .load(url)
+//            .into(this)
+//    }
+//}

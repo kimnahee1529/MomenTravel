08701d8ea228e1f3ea3d5cc8058c80d7c4f9d34b
nhkim <nhkim1529@gmail.com>
2024-02-07 10:49:58 +0900

    Feat: 변수/viewModel 초기화

--- app/src/main/java/com/android/traveltube/home/HomeFragment.kt
+++ app/src/main/java/com/android/traveltube/home/HomeFragment.kt
@@ -1,20 +1,45 @@
 package com.android.traveltube.home
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.traveltube.R
+import androidx.fragment.app.viewModels
+import com.android.traveltube.databinding.FragmentHomeBinding
+import com.android.traveltube.factory.HomeViewModelFactory
+import com.android.traveltube.factory.PreferencesRepository
+import com.android.traveltube.repository.YoutubeRepository
 
 
-class HomeFragment : Fragment() {
+class HomeFragment() : Fragment() {
+    private val favoriteKey = "loadYoutubeData"
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: HomeViewModel by viewModels {
+        val preferences = requireContext().getSharedPreferences(favoriteKey, Context.MODE_PRIVATE)
+        HomeViewModelFactory(YoutubeRepository(), PreferencesRepository(preferences))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    //video, search api 호출
+    private fun initView(){
+//        viewModel.loadTrendingVideos("KR")
+//        viewModel.loadSearchingVideos("KR")
     }
 
 }

41498bcf9ff522f9a5451cf26a2adfe4822e5d04
nhkim <nhkim1529@gmail.com>
2024-02-07 15:02:45 +0900

    Feat: HomeFragment 코드 작성

--- app/src/main/java/com/android/traveltube/home/HomeFragment.kt
+++ app/src/main/java/com/android/traveltube/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.android.traveltube.home
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.android.traveltube.databinding.FragmentHomeBinding
+import androidx.fragment.app.viewModels
+import com.android.traveltube.factory.HomeViewModelFactory
+import com.android.traveltube.factory.PreferencesRepository
+import com.android.traveltube.repository.YoutubeRepository
 
-
-class HomeFragment : Fragment() {
+class HomeFragment() : Fragment() {
+    private val favoriteKey = "loadYoutubeData"
     private var _binding: FragmentHomeBinding? = null
-    private val binding: FragmentHomeBinding get() = _binding!!
+    private val binding get() = _binding!!
+    private val viewModel: HomeViewModel by viewModels {
+        val preferences = requireContext().getSharedPreferences(favoriteKey, Context.MODE_PRIVATE)
+        HomeViewModelFactory(YoutubeRepository(), PreferencesRepository(preferences))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,12 +35,22 @@ class HomeFragment : Fragment() {
         initView()
     }
 
-    private fun initView() {
-        binding.btSendData.setOnClickListener {
-            // 동영상 클릭 했을 때 상세 화면 이동
-            val action = HomeFragmentDirections.actionFragmentHomeToFragmentVideoDetail("youtube_id")
-            findNavController().navigate(action)
-        }
+//    private fun initView() {
+//        binding.btSendData.setOnClickListener {
+//            // 동영상 클릭 했을 때 상세 화면 이동
+//            val action = HomeFragmentDirections.actionFragmentHomeToFragmentVideoDetail("youtube_id")
+//            findNavController().navigate(action)
+//        }
+//        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+//
+//        return binding.root
+//    }
+
+
+    //video, search api 호출
+    private fun initView(){
+//        viewModel.loadTrendingVideos("KR")
+//        viewModel.loadSearchingVideos("KR")
     }
 
     override fun onDestroyView() {

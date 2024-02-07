96de43b600ef915b2ade6446e72967a8734b5001
nhkim <nhkim1529@gmail.com>
2024-02-07 15:05:30 +0900

    Feat: HomeViewModel 코드 작성

---
+++ app/src/main/java/com/android/traveltube/home/HomeViewModel.kt
@@ -0,0 +1,41 @@
+package com.android.traveltube.home
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.traveltube.factory.PreferencesRepository
+import com.android.traveltube.repository.YoutubeRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class HomeViewModel(
+    private val youtubeRepository: YoutubeRepository,
+    private val preferencesRepository: PreferencesRepository
+): ViewModel() {
+    fun loadTrendingVideos(region: String = "KR"){
+        viewModelScope.launch {
+            kotlin.runCatching {
+                val videos = youtubeRepository.getTrendingVideos(region)
+                Log.d("videos", videos.toString())
+            }.onFailure { exception ->
+                withContext(Dispatchers.Main) {
+                    Log.e("videos Error", "Failed to fetch trending videos", exception)
+                }
+            }
+        }
+    }
+
+    fun loadSearchingVideos(region: String = "KR"){
+        viewModelScope.launch {
+            kotlin.runCatching {
+                val videos = youtubeRepository.getSearchingVideos(region)
+                Log.d("search", videos.toString())
+            }.onFailure { exception ->
+                withContext(Dispatchers.Main) {
+                    Log.e("search Error", "Failed to fetch trending videos", exception)
+                }
+            }
+        }
+    }
+}

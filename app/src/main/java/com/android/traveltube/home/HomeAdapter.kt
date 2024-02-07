d5d2c28388ab081e7338939e8d8182e557b1ef3d
nhkim <nhkim1529@gmail.com>
2024-02-07 13:08:16 +0900

    Feat: HomeAdapter 추가

--- app/src/main/java/com/android/traveltube/home/HomeAdapter.kt
+++ app/src/main/java/com/android/traveltube/home/HomeAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.android.traveltube.databinding.RecyclerviewCatVideoItemBinding
 import com.android.traveltube.utils.UtilManager.loadImage
 
 //class HomeAdapter():

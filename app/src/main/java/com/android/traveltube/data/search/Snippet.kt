259ba8e391ec3d7b8a93c326b667ec8fa5c961d6
nhkim <nhkim1529@gmail.com>
2024-02-08 11:08:08 +0900

    Feat: publishTime 타입 Date로 변경

--- app/src/main/java/com/android/traveltube/data/search/Snippet.kt
+++ app/src/main/java/com/android/traveltube/data/search/Snippet.kt
@@ -1,12 +1,14 @@
 package com.android.traveltube.data.search
 
+import java.util.Date
+
 data class Snippet(
+    val publishedAt: Date,
     val channelId: String,
-    val channelTitle: String,
+    val title: String,
     val description: String,
-    val liveBroadcastContent: String,
-    val publishTime: String,
-    val publishedAt: String,
     val thumbnails: Thumbnails,
-    val title: String
+    val channelTitle: String,
+    val liveBroadcastContent: String,
+    val publishTime: Date
 )

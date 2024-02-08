03017d505e049ed11c033a49107394aacc5d0a47
nhkim <nhkim1529@gmail.com>
2024-02-08 14:52:08 +0900

    Feat: 날짜/clipToOutline 추가

--- app/src/main/java/com/android/traveltube/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/android/traveltube/ui/home/HomeAdapter.kt
@@ -9,23 +9,34 @@ import com.android.traveltube.databinding.RecyclerviewHomeSelectVideoBinding
 import com.android.traveltube.model.VideoDetailModel
 import com.bumptech.glide.Glide
 
-class HomeAdapter(private val onItemClicked: (VideoDetailModel) -> Unit):
+class HomeAdapter(private val onItemClicked: (VideoDetailModel) -> Unit) :
     ListAdapter<VideoDetailModel, HomeAdapter.Holder>(DocumentDiffCallback()) {
 
     class DocumentDiffCallback : DiffUtil.ItemCallback<VideoDetailModel>() {
-        override fun areItemsTheSame(oldItem: VideoDetailModel, newItem: VideoDetailModel): Boolean {
+        override fun areItemsTheSame(
+            oldItem: VideoDetailModel,
+            newItem: VideoDetailModel
+        ): Boolean {
             // 객체의 고유 식별자를 비교
             return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: VideoDetailModel, newItem: VideoDetailModel): Boolean {
+        override fun areContentsTheSame(
+            oldItem: VideoDetailModel,
+            newItem: VideoDetailModel
+        ): Boolean {
             // TODO : 그냥 ==은 왜 안되는지 확인하기
             return oldItem.title == newItem.title
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding =
-            RecyclerviewHomeSelectVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            RecyclerviewHomeSelectVideoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         return Holder(binding)
     }
 
@@ -40,12 +51,13 @@ class HomeAdapter(private val onItemClicked: (VideoDetailModel) -> Unit):
             Glide.with(itemView.context)
                 .load(data.thumbNailUrl)
                 .into(binding.ivThumbnail)
-
+            binding.ivThumbnail.clipToOutline = true
             binding.tvTitle.text = data.title
             binding.tvChannelTitle.text = data.channelTitle
             binding.root.setOnClickListener {
                 onItemClicked(data)
             }
+            binding.tvDate.text = data.publishTime.toString()
         }
     }
 }

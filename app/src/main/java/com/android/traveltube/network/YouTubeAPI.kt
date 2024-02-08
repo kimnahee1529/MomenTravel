f94642b25e4f8ef31d19213b827038bdca3a52d5
nhkim <nhkim1529@gmail.com>
2024-02-08 17:08:48 +0900

    Feat: 조회수 얻기 위한 getViewCount api 추가

--- app/src/main/java/com/android/traveltube/network/YouTubeAPI.kt
+++ app/src/main/java/com/android/traveltube/network/YouTubeAPI.kt
@@ -5,11 +5,13 @@ import com.android.traveltube.data.videos.VideoModel
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val API_MAX_RESULT = 5
+private const val API_MAX_RESULT = 5 //TODO 수정 필요
 private const val API_REGION = "KR"
 private const val API_KEY = "AIzaSyDC8ZIONu13jIWzHP6ldjrrZ-uj-Az_VQk" //TODO API키 넣어주세요
-private const val CHANNEL_ID = "UC6KwCU8Y8Uw4h_Q0ptLZkqw"
-private const val SEARCH_TEXT = "플레이브 봉구"
+private const val CHANNEL_ID = "UC6KwCU8Y8Uw4h_Q0ptLZkqw" //TODO 수정 필요
+private const val SEARCH_TEXT = "플레이브 봉구" //TODO 수정 필요
+private const val VIDEO_ID = "gbxCTwgKcX8" //TODO 수정 필요
+
 
 interface YouTubeAPI {
     /*
@@ -22,6 +24,24 @@ interface YouTubeAPI {
         part: String = "snippet",
         @Query("chart")
         chart: String = "mostPopular",
+        @Query("maxResults")
+        maxResults: Int = API_MAX_RESULT,
+        @Query("regionCode")
+        regionCode: String = API_REGION,
+        @Query("key")
+        apiKey: String = API_KEY
+    ): VideoModel
+
+    /*
+    Videos: list
+    동영상 조회수 가져오는 api
+    */
+    @GET("videos")
+    suspend fun getViewCount(
+        @Query("part")
+        part: String = "snippet,statistics",
+        @Query("id")
+        id: String = VIDEO_ID,
         @Query("maxResults")
         maxResults: Int = API_MAX_RESULT,
         @Query("regionCode")
@@ -54,7 +74,7 @@ interface YouTubeAPI {
     @GET("search")
     suspend fun getChannelsVideo(
         @Query("part")
-        part: String = "snippet",
+        part: String = "snippet,statistics",
         @Query("channelId")
         channelId: String = CHANNEL_ID,
         @Query("maxResults")
